9c194caa14a9f857944013f94494929b558e076c
infinity-dream-code <infinity-dream-code@gmail.com>
2025-08-08 15:11:40 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiBarangController;
 
+// API Barang
 Route::post('/barang', [ApiBarangController::class, 'store']);
 Route::get('/barang/search/{query}', [ApiBarangController::class, 'search']);
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\TransaksiController;
 
-
+// Halaman Kasir
 Route::get('/', [TransaksiController::class, 'index'])->name('kasir.index');
 Route::post('/kasir', [TransaksiController::class, 'store'])->name('kasir.store');
 
+// Daftar Barang
 Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
+
+// Daftar Transaksi
 Route::get('/transaksi', [TransaksiController::class, 'indextransaksi'])->name('transaksi.index');
 Route::get('/transaksi/{id}/detail', [TransaksiController::class, 'ajaxDetail']);
